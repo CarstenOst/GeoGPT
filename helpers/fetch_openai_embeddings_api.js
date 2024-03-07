0bc2651646c0536ea55ccd81afd2c5eea1d80007
Carsten Østergaard <carsteno@uia.no>
2024-03-07 08:38:10 +0100

    Now with logger

--- helpers/fetch_openai_embeddings_api.js
+++ helpers/fetch_openai_embeddings_api.js
@@ -1,7 +1,12 @@
 const config = require('../config.js');
 const { api: {model, openai_embedding_api_key} } = config;
+const fs = require('fs');
+const path = require('path');
+
+
 
 const fetchOpenAIEmbeddings = async (inputText) => {
+  appendToCsv(inputText);
   const response = await fetch('https://api.openai.com/v1/embeddings', {
     method: 'POST',
     headers: {
@@ -23,3 +28,15 @@ const fetchOpenAIEmbeddings = async (inputText) => {
 };
 
 module.exports = { fetchOpenAIEmbeddings };
+
+
+function appendToCsv(stringInput){
+  fs.appendFile('./sokLog.csv', stringInput, (err) => {
+    if (err) {
+      console.error('Error appending to the CSV file:', err);
+      return;
+    }
+
+    console.log('Successfully appended to the CSV file.');
+  });
+}
