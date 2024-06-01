71a9599f7286a8b81dd1e2efac2472ad9b9b53b9
Carsten Østergaard <carsteno@uia.no>
2024-06-01 14:36:04 +0200

    reformatted code to fit standard

--- helpers/fetch_openai_embeddings_api.js
+++ helpers/fetch_openai_embeddings_api.js
@@ -1,43 +1,42 @@
 const fetch = require("node-fetch");
 const config = require('../config.js');
-const { api: {model, openai_embedding_api_key} } = config;
+const {api: {model, openai_embedding_api_key}} = config;
 const fs = require('fs');
 
 
-
 const fetchOpenAIEmbeddings = async (inputText) => {
-  //appendToCsv(inputText);
-  const response = await fetch('https://api.openai.com/v1/embeddings', {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${openai_embedding_api_key}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      input: inputText,
-      model: model,
-      encoding_format: "float"
-    })
-  });
+    //appendToCsv(inputText);
+    const response = await fetch('https://api.openai.com/v1/embeddings', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${openai_embedding_api_key}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            input: inputText,
+            model: model,
+            encoding_format: "float"
+        })
+    });
 
-  if (!response.ok) {
-    const errorBody = await response.text();
-    throw new Error(`Network response failed: ${response.status} ${response.statusText} - ${errorBody}`);
-  }
-
-  return response.json();
-};
-
-module.exports = { fetchOpenAIEmbeddings };
-
-
-function appendToCsv(stringInput){
-  fs.appendFile('./sokLog.csv', stringInput + '\n', (err) => {
-    if (err) {
-      console.error('Error appending to the CSV file:', err);
-      return;
+    if (!response.ok) {
+        const errorBody = await response.text();
+        throw new Error(`Network response failed: ${response.status} ${response.statusText} - ${errorBody}`);
     }
 
-    console.log('Successfully appended to the CSV file.');
-  });
+    return response.json();
+};
+
+module.exports = {fetchOpenAIEmbeddings};
+
+
+function appendToCsv(stringInput) {
+    fs.appendFile('./sokLog.csv', stringInput + '\n', (err) => {
+        if (err) {
+            console.error('Error appending to the CSV file:', err);
+            return;
+        }
+
+        console.log('Successfully appended to the CSV file.');
+    });
 }
